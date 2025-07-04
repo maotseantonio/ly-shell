014214f4e1319abad5d3be96d2ad727b1c8e1a0f
maotseantonio <thetzinantonio@gmail.com>
2025-07-04 18:09:13 +0630

    update-flake

--- flake.nix
+++ flake.nix
@@ -1,6 +1,5 @@
-
 {
-  description = "QuickShell QS-Bar flake with home config and activation";
+  description = "QuickShell QS-Bar flake with home config, activation, and extra runtime deps";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -10,68 +9,93 @@
     };
   };
 
-  outputs = { self, nixpkgs, quickshell, ... }: let
-    forAllSystems = f: nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" ] f;
-  in {
-    packages = forAllSystems (system: let
-      pkgs = import nixpkgs { inherit system; };
+  outputs = { self, nixpkgs, quickshell, ... }:
+    let
+      forAllSystems = f: nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" ] f;
+    in
+    {
+      packages = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
 
-      quickshellBin = quickshell.packages.${system}.default;
+          quickshellBin = quickshell.packages.${system}.default;
 
-      quickshellConfig = pkgs.stdenvNoCC.mkDerivation {
-        pname = "quickshell-config";
-        version = "1.0";
-        src = ./config;
-        installPhase = ''
-          mkdir -p $out
-          cp -r * $out/
-        '';
-      };
+          extraDeps = with pkgs; [
+            libsForQt5.qtstyleplugin-kvantum
+            kdePackages.qtstyleplugin-kvantum
+            wlsunset
+            libsForQt5.qt5.qtgraphicaleffects
+            kdePackages.qt5compat
+            kdePackages.qtbase
+            kdePackages.qtdeclarative
+            kdePackages.qtmultimedia
+            libqalculate
+            colloid-kde
+            kdePackages.qqc2-desktop-style
+            kdePackages.sonnet
+            kdePackages.kirigami
+            kdePackages.kirigami-addons
+            kdePackages.breeze
+          ];
 
+          qt6Qml = pkgs.lib.concatMapStringsSep ":" (pkg: "${pkg}/lib/qt-6/qml") extraDeps;
+          qt5Qml = "${pkgs.libsForQt5.qtstyleplugin-kvantum}/lib/qt-5/qml";
+          qmlPath = "${qt6Qml}:${qt5Qml}";
 
-      qsBar = pkgs.stdenvNoCC.mkDerivation {
-  pname = "qs-bar";
-  version = "1.0";
+          quickshellConfig = pkgs.stdenvNoCC.mkDerivation {
+            pname = "quickshell-config";
+            version = "1.0";
+            src = ./config;
+            installPhase = ''
+              mkdir -p $out
+              cp -r * $out/
+            '';
+          };
 
-  nativeBuildInputs = [ pkgs.makeWrapper ];
+          qsBar = pkgs.stdenvNoCC.mkDerivation {
+            pname = "qs-bar";
+            version = "1.0";
 
-  unpackPhase = "true";  # no source to unpack
+            nativeBuildInputs = [ pkgs.makeWrapper ];
 
-  installPhase = ''
-    mkdir -p $out/bin
+            # No src, so disable unpackPhase
+            unpackPhase = "true";
 
-    makeWrapper ${quickshellBin}/bin/quickshell $out/bin/qs-bar \
-      --set QML2_IMPORT_PATH "${qmlPath}" \
-      --prefix PATH : ${pkgs.lib.makeBinPath extraDeps} \
-      --add-flags "-p $HOME/.config/quickshell/shell.qml"
-  '';
-};
+            installPhase = ''
+              mkdir -p $out/bin
 
+              makeWrapper ${quickshellBin}/bin/qs $out/bin/qs-bar \
+                --set QML2_IMPORT_PATH "${qmlPath}" \
+                --prefix PATH : ${pkgs.lib.makeBinPath extraDeps} \
+                --add-flags "-p $HOME/.config/quickshell/shell.qml"
+            '';
+          };
+        in
+        {
+          quickshellConfig = quickshellConfig;
+          qs-bar = qsBar;
+          default = qsBar;
+        }
+      );
 
-    in {
-      quickshellConfig = quickshellConfig;
-      qs-bar = qsBar;
-      default = qsBar;
-    });
+      homeManagerModules.quickshell = { config, lib, pkgs, ... }: let
+        cfg = config.myQuickshell;
+      in {
+        options.myQuickshell = {
+          enable = lib.mkEnableOption "QuickShell bar with home config";
+        };
 
-    homeManagerModules.quickshell = { config, lib, pkgs, ... }: let
-      cfg = config.myQuickshell;
-    in {
-      options.myQuickshell = {
-        enable = lib.mkEnableOption "QuickShell bar with home config";
-      };
+        config = lib.mkIf cfg.enable {
+          home.packages = [
+            self.packages.${pkgs.system}.qs-bar
+          ];
 
-      config = lib.mkIf cfg.enable {
-        home.packages = [
-          self.packages.${pkgs.system}.qs-bar
-        ];
-
-        home.activation.setupQuickshell = config.lib.dag.entryAfter ["writeBoundary"] ''
-          mkdir -p "$HOME/.config/quickshell"
-          cp -r --no-preserve=all ${self.packages.${pkgs.system}.quickshellConfig}/* "$HOME/.config/quickshell"
-          chmod -R u+w "$HOME/.config/quickshell"
-        '';
+          home.activation.setupQuickshell = config.lib.dag.entryAfter ["writeBoundary"] ''
+            mkdir -p "$HOME/.config/quickshell"
+            cp -r --no-preserve=all ${self.packages.${pkgs.system}.quickshellConfig}/* "$HOME/.config/quickshell"
+            chmod -R u+w "$HOME/.config/quickshell"
+          '';
+        };
       };
     };
-  };
 }
